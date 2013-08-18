777ce8b1e03246ff545a519ffc35a63fb4f3072a
augustin merle <augustin@laveaucoupet.fr>
2013-08-18 22:53:42 +0200

    remove stuff

--- src/Atc/Bundle/HouseShareBundle/Controller/DefaultController.php
+++ src/Atc/Bundle/HouseShareBundle/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace Atc\Bundle\HouseShareBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
